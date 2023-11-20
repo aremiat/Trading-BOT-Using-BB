093754cfb229d2f3cbdb4edbf7d01227fc2ea022
aremiat <134190489+aremiat@users.noreply.github.com>
2023-11-20 17:05:54 +0100

    Add files via upload

---
+++ Forex Trading Bot Using BB.py
@@ -0,0 +1,36 @@
+class CasualBrownDonkey(QCAlgorithm):
+
+    def Initialize(self):
+        self.SetStartDate(2015, 1, 1)  # Set Start Date
+        self.SetEndDate(2021, 1, 1)
+        self.SetCash(100000)  # Set Strategy Cash
+
+        self.eurusd = self.AddForex("EURUSD", Resolution.Daily, Market.Oanda).Symbol
+
+        self.bb =self.BB(self.eurusd, 20, 2)
+
+       
+def OnData(self, Data):
+
+        if not self.bb.IsReady:
+            return
+
+
+
+        price = self.Securities[self.eurusd].Price
+
+ 
+        if price > self.bb.UpperBand.Current.Value:
+            if not self.Portfolio[self.eurusd].IsShort:
+                self.SetHoldings(self.eurusd, -1)
+
+
+        elif price < self.bb.LowerBand.Current.Value:
+            if not self.Portfolio[self.eurusd].IsLong:
+                self.SetHoldings(self.eurusd, 1)
+
+        else:
+            if price < self.bb.LowerBand.Current.Value and self.Portfolio[self.eurusd].IsShort:
+                self.Liquidate()
+            elif price > self.bb.UpperBand.Current.Value and self.Portfolio[self.eurusd].IsLong:
+                self.Liquidate()
